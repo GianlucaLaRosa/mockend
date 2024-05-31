a42bb18ac76b0139d5ee1d7e8b862469e613e9cc
Gianluca La Rosa <gianluca_larosa@hotmail.com>
2024-05-31 16:03:11 +0200

    add followers

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const apiUrl = 'https://mockend.com/api/GianlucaLaRosa/mockend/';
+const secondApiUrl = 'https://mockend.com/api/GianlucaLaRosa/mockend/tree/another-configuration/'
 
 // Fetch all posts
-async function fetchItems() {
-  const response = await fetch(`${apiUrl}bitrockers`);
+async function fetchItems(sortingQuery) {
+  const response = await fetch(`${apiUrl}bitrockers?${sortingQuery}`);
   const items = await response.json();
   displayItems(items);
 }
@@ -11,12 +12,13 @@ async function fetchItems() {
 function displayItems(items) {
   const itemList = document.getElementById('cards');
   items.forEach(item => {
+    const avatarUrl = isValidURL(item.avatar) ? item.avatar : `./assets/${item.avatar}.png`
     const div = document.createElement('div');
     div.style.backgroundColor = item.backgroundColor+11;
     div.className = 'card';
     div.innerHTML = `
       <div class="card__header">
-        <div class="card__header__avatar"><img src="./assets/${item.avatar}.png"></div>
+        <div class="card__header__avatar"><img src="${avatarUrl}"></div>
         <div class="card__header__title">${item.name} ${item.surname}</div>
         <div class="card__header__hometown">from ${item.homeTown}<span>(${item.country})</span></div>
       </div>
@@ -28,7 +30,7 @@ function displayItems(items) {
         <div class="card__body__repo">personal website: ${item.site}, access password: ${item.password}</div>
       </div>
       <div class="card__footer">
-        <button onclick="deleteItem('${item._id}')">Delete</button>
+        <button onclick="deleteItem('${item.id}')">Delete</button>
       </div>
     `;
     itemList.appendChild(div);
@@ -69,11 +71,31 @@ async function updateItem(id) {
 
 // Delete an item
 async function deleteItem(id) {
-  await fetch(`${apiUrl}/${id}`, {
+  await fetch(`${apiUrl}bitrockers/${id}`, {
     method: 'DELETE'
   });
   fetchItems();
 }
 
+function isValidURL(url) {
+  try {
+    url = new URL(url);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
+
+function ascSort() {
+  const itemList = document.getElementById('cards');
+  itemList.innerHTML = '';
+  fetchItems("name_order=asc")
+}
+
+function descSort() {
+  const itemList = document.getElementById('cards');
+  itemList.innerHTML = '';
+  fetchItems("name_order=desc")
+}
 // Initial fetch of items
 fetchItems();
